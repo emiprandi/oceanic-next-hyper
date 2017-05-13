6aa1dfb7a18fa2a4d9318397e15533a54abdde18
Emiliano Prandi <emiprandi@gmail.com>
2017-05-13 00:54:42 -0300

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+exports.decorateConfig = config => Object.assign(config, {
+  foregroundColor: '#D8DEE9',
+  backgroundColor: '#1B2B34',
+  borderColor: '#343D46',
+  cursorShape: 'BEAM',
+  colors: {
+    black: '#1B2B34',
+    red: '#EC5f67',
+    green: '#99C794',
+    yellow: '#FAC863',
+    blue: '#6699CC',
+    magenta: '#C594C5',
+    cyan: '#5FB3B3',
+    white: '#CDD3DE',
+    lightBlack: '#808080',
+    lightRed: '#EC5f67',
+    lightGreen: '#99C794',
+    lightYellow: '#FAC863',
+    lightBlue: '#6699CC',
+    lightMagenta: '#C594C5',
+    lightCyan: '#5FB3B3',
+    lightWhite: '#D8DEE9'
+  }
+});
